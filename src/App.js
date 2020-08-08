5e867f9afff8307cdaa3e4f22a01a2d255a050b7
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-07 23:15:59 -0700

    add navbar to page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar.js';
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
