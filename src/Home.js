d9c45d532197018773de5e85051add4f6a234290
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-10 16:11:01 -0700

    create home page

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import headshot from './headshot.jpeg';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="headshot">
+          <img src={headshot} alt="" />
+        </div>
+        <div className="home-name">Hey, I'm Jordan</div>
+        <div className="home-header">Self taught Full Stack Engineer</div>
+        <div className="home-intro">
+          I have always had a love for programming. Although my professional
+          career has been focused on Project Management and Training, there has
+          always been a part of me itching to program. I am hungry to work on
+          interesting projects and work with teams that iterate quickly.
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
