895e55c0d443b769cc69f51f162e75365d209cb2
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-16 14:17:44 -0700

    add new resume

--- src/ResumePage.js
+++ src/ResumePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import resume from './JTS_Resume.pdf';
+import resume from './JTS_Resume_Portfolio.pdf';
 import {Document, Page, pdfjs} from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
