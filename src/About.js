86a4f87f50e6be883ea1a4eeec0c4896aacb8964
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-08-16 15:59:08 -0700

    add mobile styling for about me page

--- src/About.js
+++ src/About.js
@@ -6,33 +6,41 @@ import jsIcon from './javascript.png';
 import mongoIcon from './mongodb.png';
 import awsIcon from './aws.png';
 import htmlIcon from './html5.png';
+import node from './nodejs.png';
 
 class About extends Component {
-  //image headshot
-  //about me
-  //skills
   render() {
     return (
       <div>
         <div className="headshot">
           <img src={headshot} alt=""></img>
+          <div className="about-me-intro-container">
+            <div className="about-me-name">Jordan Suero</div>
+            <div className="about-me-title">Full Stack Engineer</div>
+          </div>
         </div>
-        <div className="about-me-header">About Me / Skills</div>
         <div className="about-skills-container">
           <div className="about-container">
-            Although my professional career has been focused on Project
-            Management and Training, there has always been an itch to program.
-            While working I built my skillset through multiple personal projects
-            and a supportive team of engineers. I am hungry to work on
-            interesting projects and work with teams that iterate quickly.
+            <div className="about-me-header">About Me</div>
+            <div className="skills-content">
+              Although my professional career has been focused on Project
+              Management and Training, there has always been an itch to program.
+              While working, I built my skillset through multiple personal
+              projects and a supportive team of engineers. I am hungry to work
+              on interesting projects and work with teams that iterate quickly.
+            </div>
           </div>
-          <div className="skills-container">
-            <img src={jsIcon} alt=""></img>
-            <img src={reactIcon} alt=""></img>
-            <img src={cssIcon} alt=""></img>
-            <img src={htmlIcon} alt=""></img>
-            <img src={mongoIcon} alt=""></img>
-            <img src={awsIcon} alt=""></img>
+          <div className="about-container">
+            <div className="about-me-header">Skills</div>
+            <div className="skills-content">
+              <img src={jsIcon} alt=""></img>
+              <img src={reactIcon} alt=""></img>
+              <img src={node} alt=""></img>
+              <img src={cssIcon} alt=""></img>
+              <img src={htmlIcon} alt=""></img>
+              <img src={mongoIcon} alt=""></img>
+              <img src={awsIcon} alt=""></img>
+            </div>
           </div>
         </div>
       </div>
