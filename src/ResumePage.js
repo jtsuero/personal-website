fc09082a95adf92ff857987424efd99553d29bbd
Jordan Suero <9836321+jtsuero@users.noreply.github.com>
2020-09-16 13:52:48 -0700

    add state to keep track of current resume page

--- src/ResumePage.js
+++ src/ResumePage.js
@@ -4,11 +4,18 @@ import {Document, Page, pdfjs} from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 class ResumePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pageNumber: 1,
+    };
+  }
   render() {
     return (
       <div className="resume-container">
         <Document file={resume} className="resume-document">
-          <Page pageNumber={1} className="resume-page" />
+          <Page pageNumber={this.state.pageNumber} className="resume-page" />
+          <p>Page {this.state.pageNumber} of 2</p>
         </Document>
       </div>
     );
